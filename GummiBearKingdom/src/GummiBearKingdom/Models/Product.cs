e8da758967e7e8ac9f91d6e65116f71648adbda0
Charles Emrich <libertinistic@gmail.com>
2017-08-06 22:31:05 -0700

    Add Product Class and Models folder

---
+++ GummiBearKingdom/src/GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Origin { get; set; }
+    }
+}
